0c97e5a3cec6d245cd03ddced0ba7a8f457c9c2f
Najm-Ur-Rehman <67186639+Najaam@users.noreply.github.com>
2025-05-01 04:15:53 -0700

    .

--- Server/server.js
+++ Server/server.js
@@ -1,19 +1,35 @@
 const express = require('express');
-const socketIO = require('socket.io');
 const cors = require('cors');
 const db = require("./Db");
-const authroute = require("./Routes/Authroutes")
-const protectedroute = require("./Routes/Protectedroute")
+const authroute = require("./Routes/Authroutes");
+const protectedroute = require("./Routes/Protectedroute");
 const app = express();
 const { port } = require('./config');
 const PORT = port;
 
-
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Backend Running'));
+// Root route
+app.get('/', (req, res) => {
+  res.status(200).send({
+    message: "Welcome to the backend of Jade OS! Everything is running smoothly.",
+  });
+});
+
+// Routes
 app.use('', authroute);
 app.use('/protected', protectedroute);
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// Handle undefined routes
+app.use((req, res) => {
+  res.status(404).send({
+    error: "The requested resource was not found on this server.",
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
