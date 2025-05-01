7eee2c431adeed89b85879d461858026ab89ac59
Najm-Ur-Rehman <67186639+Najaam@users.noreply.github.com>
2025-05-01 12:05:28 -0700

    seperating server and client

--- Server/Routes/Protectedroute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verifyToken = require('../Middlewares/Authmiddleware');
-// Protected route
-router.get('/', verifyToken, (req, res) => {
-res.status(200).json({ message: 'Protected route accessed' });
-});
-
-module.exports = router;
--- Server/config.js
+++
@@ -1,7 +0,0 @@
-
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-    port: process.env.PORT,
-    dburl: process.env.DB_URL,
-};
